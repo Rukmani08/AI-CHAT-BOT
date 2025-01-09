ac9cbe0afd3b12180c115a2edb74868946fae67d
Rukmani Kanwar <91824494+Rukmani08@users.noreply.github.com>
2025-01-09 16:41:31 +0530

    Add files via upload

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import app from  "./app.js";
+import { connectionToDatabase } from "./db/connection.js";
+
+//connection and listners
+const PORT=process.env.PORT || 3000;
+connectionToDatabase()
+.then(()=>{
+
+  app.listen( PORT, ()=> console.log("server open & Connected to Database🚀 "))
+})
+.catch((err)=> console.log(err))
+
