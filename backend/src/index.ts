1e33a9c24f84c44394f095abc823dd48999553f3
Rukmani Kanwar <91824494+Rukmani08@users.noreply.github.com>
2025-01-15 15:37:36 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,6 @@
 import app from  "./app.js";
 import { connectionToDatabase } from "./db/connection.js";
-import express from 'express';
-import path from 'path';
 
-const app = express();
-
-app.use(express.static(path.join(__dirname, './backend/src')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../index.html'));
-});
 
 //connection and listners
 const PORT=process.env.PORT || 3000;
