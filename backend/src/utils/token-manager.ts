f5108de5403bf84e7a264a5fcef04478f3415699
Rukmani Kanwar <91824494+Rukmani08@users.noreply.github.com>
2025-01-27 11:23:11 +0530

    Update token-manager.ts

--- backend/src/utils/token-manager.ts
+++ backend/src/utils/token-manager.ts
@@ -3,10 +3,10 @@ import jwt from 'jsonwebtoken'
 import { COOKIE_NAME } from './constants.js';
 
 
-export const createToken= (id: string, email: string, expiresIn: string)=>{
+export const createToken= (id: string, email: string, expiresIn: jwt.StringValue)=>{
     const payload= {id, email}
     const token = jwt.sign(payload, process.env.JWT_SECRET, {
-        expiresIn:  expiresIn+'7d',
+        expiresIn,
     });
 
     return token;
