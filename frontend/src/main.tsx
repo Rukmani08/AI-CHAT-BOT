b15f28107c07c170cfc9982fe8fa3f16a9164f40
Rukmani Kanwar <91824494+Rukmani08@users.noreply.github.com>
2025-02-05 12:19:46 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.tsx'
 import axios from "axios";
 import { Toaster } from 'react-hot-toast'
-axios.defaults.baseURL = process.env.REACT_APP_API_URL  ;
+axios.defaults.baseURL = "http://localhost:3000/api/v1" ;
 axios.defaults.withCredentials = true;
 
 const theme = createTheme({
