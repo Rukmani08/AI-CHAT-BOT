53ae505d361f1d7f9f067ef4560bd863080a9c98
Rukmani Kanwar <91824494+Rukmani08@users.noreply.github.com>
2025-02-08 12:46:11 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,8 @@ import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.tsx'
 import axios from "axios";
 import { Toaster } from 'react-hot-toast'
-axios.defaults.baseURL = "http://localhost:3000/api/v1" ;
+axios.defaults.baseURL = "https://ai-chat-bot-kedd.onrender.com/api/v1"
+// axios.defaults.baseURL = "http://localhost:3000/api/v1" ;
 axios.defaults.withCredentials = true;
 
 const theme = createTheme({
